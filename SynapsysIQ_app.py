f6792a237abd929039b9bbcc3d494985d35396ac
Sharif <sharifimam@hotmail.co.uk>
2025-11-08 15:05:06 +0000

    Initial commit
    Step 1: data load and initial cleaning

---
+++ SynapsysIQ_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+
+
+st.title("Synapsys IQ Product Analytics Dashboard")
+st.sidebar.title("Synapsys IQ Product Analytics Dashboard")
